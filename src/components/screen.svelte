34a8f7f1fd9bff6e4a466ba11ae01c8ec811601e
Navin Kodag <navinkodag@gmail.com>
2021-06-28 00:31:55 +0530

    🐳

---
+++ src/components/screen.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import Phaser from 'phaser';
+
+	var config = {
+		type: Phaser.AUTO,
+		width: window.innerHeight,
+		height: window.innerWidth,
+		physics: {
+			default: 'arcade',
+			arcade: {
+				gravity: { y: 200 }
+			}
+		},
+		scene: {
+			preload: preload,
+			create: create
+		}
+	};
+
+	var game: Phaser.Game = new Phaser.Game(config);
+	window.focus();
+	function preload() {
+		this.load.image('morty', '/static/rick_morty_sprite.png');
+		this.load.image('bg', '/static/landscape.jpg');
+		this.load.svg('platform', '/static/platform.svg');
+	}
+	function create() {
+		this.add.image(400, 300, 'bg');
+		let morty_sprite = this.game.add.sprite(300, 200, 'morty');
+		morty_sprite.name = 'morty';
+		this.game.physics.enable(morty_sprite, Phaser.AUTO);
+
+		// var emitter = particles.createEmitter({
+		// 	speed: 10,
+		// 	scale: { start: 1, end: 0 },
+		// 	blendMode: 'ADD'
+		// });
+
+		var logo = this.physics.add.image(200, 10, 'morty');
+
+		logo.setVelocity(100, 200);
+		logo.setBounce(1, 1);
+		logo.setCollideWorldBounds(true);
+
+		// emitter.startFollow(logo);
+	}
+</script>
---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	let screen: any;
+
+	onMount(async () => {
+		screen = (await import('../components/screen.svelte')).default;
+	});
+</script>
+
+<svelte:component this={screen} />
