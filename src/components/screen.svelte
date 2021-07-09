21619cd65bbc881ad8ebcfd2d3da4488d97dd952
Navin Kodag <navinkodag@gmail.com>
2021-07-09 09:24:33 +0530

    chore: add remote

--- src/components/screen.svelte
+++ src/components/screen.svelte
@@ -12,31 +12,23 @@
 			}
 		},
 		scene: {
-			preload: preload,
+			preload: reload,
 			create: create
 		}
 	};
 
 	var game: Phaser.Game = new Phaser.Game(config);
 	window.focus();
-	function preload() {
+	function reload() {
 		this.load.image('morty', '/static/rick_morty_sprite.png');
 		this.load.image('bg', '/static/landscape.jpg');
-		this.load.svg('platform', '/static/platform.svg');
 	}
 	function create() {
 		this.add.image(400, 300, 'bg');
 		let morty_sprite = this.game.add.sprite(300, 200, 'morty');
-		morty_sprite.name = 'morty';
 		this.game.physics.enable(morty_sprite, Phaser.AUTO);
 
-		// var emitter = particles.createEmitter({
-		// 	speed: 10,
-		// 	scale: { start: 1, end: 0 },
-		// 	blendMode: 'ADD'
-		// });
-
-		var logo = this.physics.add.image(200, 10, 'morty');
+		const logo = this.physics.add.image(200, 10, 'morty');
 
 		logo.setVelocity(100, 200);
 		logo.setBounce(1, 1);
